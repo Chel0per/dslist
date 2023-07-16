9f065237b8e36436f86002df5d445eb081ba9f16
Marcelo <marcelo.smarques7@gmail.com>
2023-07-16 11:16:34 -0300

    Move position belonging table

--- src/main/java/com/chel0per/dslist/services/GameListService.java
+++ src/main/java/com/chel0per/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.chel0per.dslist.dto.GameListDTO;
 import com.chel0per.dslist.entities.GameList;
+import com.chel0per.dslist.projections.GameMinProjection;
 import com.chel0per.dslist.repositories.GameListRepository;
+import com.chel0per.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,11 +18,28 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;
+	}
+	
+	@Transactional
+	public void moveGame(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection removedGame = list.remove(sourceIndex);
+		list.add(destinationIndex,removedGame);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i<= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 	
 }
